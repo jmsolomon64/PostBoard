3e9e087e7e2156f511df08d6ba20116b31019476
Jacob Solomon <jmsolomon64@gmail.com>
2022-08-20 04:43:24 -0400

    Initial develop

---
+++ PostBoard/Client/Shared/Post/PostCreate.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostBoard.Client.Shared.Post
+{
+    public class PostCreate
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+    }
+}
